f3ff49a3395f70a0b4bea3a11d35ef8239e1bb18
Josh McIntyre <josh.r.mcintyre13@gmail.com>
2017-11-08 18:29:37 -0800

    Create Project, Add Libraries

---
+++ Projects/Project 2/Uno/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Josh McIntyre
+ * Created on November 8, 2017, 6:25 PM
+ * Purpose: Recreation of the popular card game Uno
+ */
+
+//Function Prototypes
+
+//Global Constants
+
+//User Libraries
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+
+    
+    //Exits Program
+    return 0;
+}
+
